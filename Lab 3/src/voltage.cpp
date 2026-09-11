84f083f7512cedec40f69de9213cf388276f37f6
esloat <ethan@sloatcentral.com>
2026-09-11 15:00:49 -0400

    Initial lab work

--- Lab 3/src/voltage.cpp
+++ Lab 3/src/voltage.cpp
@@ -1,10 +1,11 @@
+
 #include <Arduino.h>
 
 // function prototype
 int voltage(float analogvalue);
 
 // Put your potentiometer pin assignment here
-const int sensorPin = 0;
+const int sensorPin = A0;
 
 void setup() {
     Serial.begin(115200);
@@ -12,8 +13,11 @@ void setup() {
 
 void loop() {
     // This is the line for printing in the serial
-    Serial.println(voltage(analogRead(sensorPin)));
-    delay(50); 
+    int rawPotValue = analogRead(sensorPin); // ecs181
+    int voltageValue = voltage(rawPotValue); // ecs181
+    Serial.println(voltageValue);
+    Serial.println(rawPotValue); //ecs181 - confirming that the potentiometer value is correct for the outputted voltage
+    delay(5000); //ecs181 - trying to only sample every 5 second to allow to confirm value with oscilloscope if desired
 }
 
 // function to calculate output voltage
@@ -21,5 +25,9 @@ int voltage(float analogvalue){
     int voltage;
     //analogRead(sensorPin); gives us 0-4095 values
     //use the formula (Analog value*Reference voltage) / (Sensor Resolution) to calculate the output voltage
+    //max voltage (reference) is 3V
+    const int refVoltage = 3; //ecs181 - I prefer to work with variables
+    const int maxAnalogValue = 4095; //ecs181
+    voltage = (analogvalue*refVoltage)/maxAnalogValue; //ecs181
     return voltage;
 }
